473ca78f4aea48523f355453807547667378a5e6
hungnguyen <hungnguyen81191@gmail.com>
2023-11-30 03:36:17 +0700

    install and apply vue3 cookie

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
 import router from './routes/indexRoute';
+import { createPinia } from 'pinia';
+import VueCookies from 'vue3-cookies';
 
 const app = createApp(App);
-app.use(router).mount('#app');  
+app.use(VueCookies, {
+    expireTimes: "30d",
+    path: "/",
+    domain: "",
+    secure: true,
+    sameSite: "None"
+})
+    .use(router)
+    .use(createPinia())
+    .mount('#app');  
